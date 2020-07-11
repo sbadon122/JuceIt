881915b1562592127b0827310003cd1b9e021ad0
Sebastian Badon <sebastianmbadon@gmail.com>
2020-07-11 01:07:48 -0400

    Adding readme and images

---
+++ README.md
@@ -0,0 +1,15 @@
+#Juce It Plugin
+
+![Plugin Demo](Images/JuceItAudioPlugin.png)
+
+##Movement
+
+![Plugin](Images/JuceItPlugin.gif)
+
+##Parameters
+
+![Plugin with Parameters](Images/JuceItParameters.gif)
+
+##Automation
+
+![Plugin with Automation](Images/JuceItAutomation.gif)
---
+++ Source/JuceItParameters.h
@@ -0,0 +1,53 @@
+/*
+  ==============================================================================
+
+    JuceItParameters.h
+    Created: 10 Jul 2020 2:45:16pm
+    Author:  Sebastian Badon
+
+  ==============================================================================
+*/
+
+#pragma once
+using namespace juce;
+
+/**
+ This header file serves as a means of tracking all parameters and its fields in the audio component.
+ */
+
+enum JuceItParameters {
+    jParameter_Frequency = 0,
+    jParameter_Resonance,
+    jParameter_TotalParamNums
+};
+
+static String JuceItParameterID [jParameter_TotalParamNums] =
+{
+    "Frequency",
+    "Resonance",
+};
+
+static String JuceItParameterLabel [jParameter_TotalParamNums] =
+{
+    "Frequency",
+    "Resonance",
+};
+
+static float JuceItParameterDefaultValue [jParameter_TotalParamNums] =
+{
+    0.75f,
+    0.65f,
+};
+
+
+static float JuceItParameterMinValue [jParameter_TotalParamNums] =
+{
+    0.0f,
+    0.0f,
+};
+
+static float JuceItParameterMaxValue [jParameter_TotalParamNums] =
+{
+    1.0f,
+    1.0f,
+};
